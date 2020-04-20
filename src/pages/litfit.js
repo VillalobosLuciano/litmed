364a68f1150737b93061b32c1e11a200e8a08d92
VillalobosLuciano <57425159+VillalobosLuciano@users.noreply.github.com>
2020-04-20 13:04:42 +1000

    test netlify forms

--- src/pages/litfit.js
+++ src/pages/litfit.js
@@ -1,13 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import ContactForm from "../components/contactForm"
+
 
 const Litfit = () => (
   <Layout>
     <SEO title="Litfit" />
     <Link to="/">Go back to the homepage</Link>
+    <ContactForm />
   </Layout>
 )
 
