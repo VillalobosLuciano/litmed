83d201ca1b4bc30ddc8651d756ab4caf13f8d75c
VillalobosLuciano <57425159+VillalobosLuciano@users.noreply.github.com>
2020-05-17 12:14:35 +1000

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -52,7 +52,7 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
     createNodeField({
-      name: `slug`,
+      name: `slug.normalize("NFD").replace(/[\u0300-\u036f]/g, "")`,
       node,
       value,
     })
